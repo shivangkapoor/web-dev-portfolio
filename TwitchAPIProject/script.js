18547e419fd767ed6a4804e03c509449794c6cf0
Michael Sharp <msharp.oh@gmail.com>
2015-09-27 10:18:14 -0400

    rebuilt twitchAPIProject

--- TwitchAPIProject/script.js
+++ TwitchAPIProject/script.js
@@ -1,73 +1,55 @@
-//Angular Controller
-var streamingApp = angular.module("TwitchEm", []);
-streamingApp.controller("MainCtrl", function($scope, $http) {
+(function() {
+    var STREAMERS = ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb",
+                          "thomasballinger","noobs2ninjas","beohoff",
+                          "RiotGamesBrazil", "FragbiteLive", "realkraftyy", "medryBW", "GfinityTV"];
+    var TWITCH_URL = "https://api.twitch.tv/kraken/";
 
-  $scope.allUsers = [];
-  $scope.onlineUsers = [];
-  $scope.offlineUsers = [];
+    var app = angular.module("TwitchEm", []);
+    app.controller("AppController", ["$http", function($http) {
+      var channels = this;
 
-  var STREAMERS = ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb",
-                        "thomasballinger","noobs2ninjas","beohoff", "brunofin", "comster404",
-                        "RiotGamesBrazil", "FragbiteLive", "realkraftyy", "medryBW"];
-  var TWITCH_URL = "https://api.twitch.tv/kraken/";
+      channels.all = [];
+      channels.online = [];
+      channels.offline = [];
 
-  STREAMERS.forEach(function(stream) {
-    var obj = {};
+      STREAMERS.forEach(function(stream) {
+        var tempStream = {};
 
-    $.getJSON(TWITCH_URL + "streams/" + stream).success(function(data) {
-      var streaming = (data.stream === null) ? false : true;
+        $http.get(TWITCH_URL + "streams/" + stream).success(function(data) {
+          var streaming = (data.stream === null) ? false : true;
+          tempStream.username = stream;
 
-      if (streaming) {
-        obj.status = "fa fa-check";
-        obj.title = data.stream.channel.status;
-      } else {
-        obj.status = "fa fa-exclamation";
-        data.title = "";
+          if(streaming) {
+            tempStream.status = "fa fa-check";
+            tempStream.title = data.stream.channel.status;
+          } else {
+            tempStream.status = "fa fa-exclamation";
+            tempStream.title = "";
+          }
+
+          $http.get(TWITCH_URL + "channels/" + stream).success(function(data) {
+            tempStream.name = data.display_name;
+            tempStream.logo = (data.logo !== null ? data.logo : "http://blog.roblox.com/wp-content/uploads/2014/02/Twitch-Icon.png");
+
+            channels.all.push(tempStream);
+            if(streaming) {
+              channels.online.push(tempStream);
+            } else {
+              channels.offline.push(tempStream);
+            }
+          });
+        });
+      });
+    }]);
+    app.controller("PanelController", function() {
+      this.tab = 0;
+
+      this.selectTab =  function(setTab) {
+        this.tab = setTab;
+      };
+
+      this.isSelected = function(checkTab) {
+        return this.tab === checkTab;
       }
-      obj.username = stream;
-
-      $.getJSON(TWITCH_URL + "users/" + stream).success(function(data) {
-        obj.name = data.display_name;
-        obj.logo = data.logo;
-
-        $scope.allUsers.push(obj);
-        if (streaming) {
-          $scope.onlineUsers.push(obj);
-        } else {
-          $scope.offlineUsers.push(obj);
-        }
-        $scope.profile = $scope.allUsers;
-        $scope.$apply();
-      });
     });
-  });
-
-  $('#menu li').on('click', function() {
-    if ($(this).data('display') === 'allUsersUsers') {
-      $scope.profile = $scope.allUsersUsers;
-    } else if ($(this).data('display') === 'onlineUsersUsers') {
-      $scope.profile = $scope.onlineUsersUsers;
-    } else {
-      $scope.profile = $scope.offlineUsersUsers;
-    }
-    $scope.$apply();
-  })
-});
-
-streamingApp.directive("errSrc", function() {
-  return {
-    link: function(scope, element, attrs) {
-      scope.$watch(function() {
-        return attrs["ngSrc"];
-      }, function(value) {
-        if(!value) {
-          element.attr("src", attrs.errSrc);
-        }
-      });
-
-      element.bind("error", function() {
-        element.attr("src", attrs.errSrc);
-      });
-    }
-  }
-});
+})();
