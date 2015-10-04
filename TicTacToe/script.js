c0cb0d7035de5e03ba9498deb4d42298c4eb9650
Michael Sharp <msharp.oh@gmail.com>
2015-10-04 13:31:55 -0400

    updated tictactoe

--- TicTacToe/script.js
+++ TicTacToe/script.js
@@ -1,20 +1,74 @@
 $(document).ready(function() {
   var playerOne = true;
+  var playerTwo = false;
+  var state = "2P";
 
-  var primaryColor = "#fff";
+  var primaryColor = "#60FF90";
   var secondaryColor = "#60CFFF";
   var tertiaryColor = "#FF60CF";
 
-  $("#board .btn").hover(
+  $("#board .tac").hover(
     function() {
-      if(playerOne) {
+      if(playerOne && !$(this).hasClass("active")) {
         $(this).css("background", primaryColor);
-      } if(!playerOne) {
-        $(this).css("background", tertiaryColor);
+        $(this).append("<i class='fa fa-times'></i>");
+      } if(playerTwo && !$(this).hasClass("active")) {
+        $(this).css("background", secondaryColor);
+        $(this).append("<i class='fa fa-circle-o'></i>");
       }
     },
     function() {
-      $(this).css("background", secondaryColor);
+      if(!$(this).hasClass("active")) {
+        $(this).css("background", tertiaryColor);
+        $(this).text("");
+      }
   });
-  
+  $("#board .tac").click(function() {
+    if(playerOne) {
+      $(this).css("background", primaryColor);
+      $(this).text("");
+      $(this).append("<i class='fa fa-times'></i>");
+      $(this).addClass("active p1");
+
+      playerOne = false;
+      playerTwo = true;
+      checkBoard();
+    } else if(playerTwo) {
+      $(this).css("background", secondaryColor);
+      $(this).text("");
+      $(this).append("<i class='fa fa-circle-o'></i>");
+      $(this).addClass("active p2");
+
+      playerOne = true;
+      playerTwo = false;
+      checkBoard();
+    }
+  });
+
+  //creates an array out of the board
+  function checkBoard() {
+    var elements = Array.prototype.slice.call($("div .tac"));
+    var tempArr = [];
+    var board = [];
+
+    elements.forEach(function(element) {
+      var temp = element.className.split(' ');
+      tempArr.push(temp);
+    });
+
+    tempArr.forEach(function(tac) {
+
+      if(tac.length < 3) {
+        board.push(0);
+      }
+      else {
+        if(tac[3] === "p1") {
+          board.push(1);
+        } else {
+          board.push(2);
+        }
+      }
+    });
+  }
+
 });
