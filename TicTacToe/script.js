6f8538907b829c1bcb8df98ba9a71debf5b33fcc
Michael Sharp <msharp.oh@gmail.com>
2015-10-05 18:15:22 -0400

    finished 2player tictactoe, added simon game skeleton

--- TicTacToe/script.js
+++ TicTacToe/script.js
@@ -2,10 +2,22 @@ $(document).ready(function() {
   var playerOne = true;
   var playerTwo = false;
   var state = "2P";
+  var turns = 0;
 
   var primaryColor = "#60FF90";
   var secondaryColor = "#60CFFF";
   var tertiaryColor = "#FF60CF";
+
+  $("#end").hide();
+
+  $("#xSelect").click(function() {
+    $("#begin").hide();
+  });
+  $("#oSelect").click(function() {
+      playerOne = false;
+      playerTwo = true;
+      $("#begin").hide();
+  });
 
   $("#board .tac").hover(
     function() {
@@ -32,7 +44,8 @@ $(document).ready(function() {
 
       playerOne = false;
       playerTwo = true;
-      checkBoard();
+      turns++;
+      checkWinner();
     } else if(playerTwo) {
       $(this).css("background", secondaryColor);
       $(this).text("");
@@ -41,15 +54,49 @@ $(document).ready(function() {
 
       playerOne = true;
       playerTwo = false;
-      checkBoard();
+      turns++;
+      checkWinner();
     }
   });
+  $("#restart").click(function() {
+    clearBoard();
+  });
+
+  function checkWinner() {
+    var board = checkBoard();
+    console.log(board + " " + turns);
+
+    //winning conditions
+    var rowOne = board[0] + board[1] + board[2];
+    var rowTwo = board[3] + board[4] + board[5];
+    var rowThree = board[6] + board[7] + board[8];
+    var colOne = board[0] + board[3] + board[6];
+    var colTwo = board[1] + board[4] + board[7];
+    var colThree = board[2] + board[5] + board[8];
+    var diagOne = board[2] + board[4] + board[6];
+    var diagTwo = board[0] + board[4] + board[8];
+
+    if(rowOne == 6 || rowTwo == 6 || rowThree == 6 ||
+          colOne == 6 || colTwo == 6 || colThree == 6 ||
+          diagOne == 6 || diagTwo == 6) {
+
+        winnerAlert(1);
+      } else if (rowOne == 15 || rowTwo == 15 || rowThree == 15 ||
+                  colOne == 15 || colTwo == 15 || colThree == 15 ||
+                  diagOne == 15 || diagTwo == 15) {
+
+          winnerAlert(2);
+      } else if(turns == 9) {
+        winnerAlert(3);
+      }
+  }
 
   //creates an array out of the board
   function checkBoard() {
     var elements = Array.prototype.slice.call($("div .tac"));
     var tempArr = [];
     var board = [];
+    var count = 0;
 
     elements.forEach(function(element) {
       var temp = element.className.split(' ');
@@ -57,18 +104,44 @@ $(document).ready(function() {
     });
 
     tempArr.forEach(function(tac) {
-
       if(tac.length < 3) {
         board.push(0);
       }
       else {
         if(tac[3] === "p1") {
-          board.push(1);
-        } else {
           board.push(2);
+        } else {
+          board.push(5);
         }
       }
     });
+    return board;
+  }
+  //Alert box for winning / tie
+  function winnerAlert(player) {
+    if(player == 3) {
+      $("#end").show();
+      $("#end p").text("It's a tie.");
+      $("#reset").click(function() {
+        clearBoard();
+      });
+    } else if(player == 1) {
+      $("#end").show();
+      $("#end p").text("Xs Win!");
+      $("#reset").click(function() {
+        clearBoard();
+      });
+    } else if(player == 2) {
+      $("#end").show();
+      $("#end p").text("Circles Win!");
+      $("#reset").click(function() {
+        clearBoard();
+      });
+    }
+  }
+  //reset board function
+  function clearBoard() {
+    location.reload(false);
   }
 
 });
