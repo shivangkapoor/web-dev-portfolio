770df961026a225ad96953d148c55322927fd504
Michael Sharp <msharp.oh@gmail.com>
2015-10-15 18:17:58 -0400

    refractor and add AI elements to TicTacToe

--- TicTacToe/script.js
+++ TicTacToe/script.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
-  var playerOne = true;
-  var playerTwo = false;
+  var exes = true;
+  var ohs = false;
+  var computerPlayer = false;
+  var playerTurn = true;
   var state = "2P";
   var turns = 0;
 
@@ -9,22 +11,32 @@ $(document).ready(function() {
   var tertiaryColor = "#FF60CF";
 
   $("#end").hide();
+  $("#begin-select").hide();
 
   $("#xSelect").click(function() {
     $("#begin").hide();
+    $("#begin-select").show();
   });
   $("#oSelect").click(function() {
-      playerOne = false;
-      playerTwo = true;
+      exes = false;
+      ohs = true;
       $("#begin").hide();
+      $("#begin-select").show();
   });
+  $("#twoPlayer").click(function() {
+    $("#begin-select").hide();
+  });
+  $("#singlePlayer").click(function() {
+    computerPlayer = true;
+    $("#begin-select").hide();
+  })
 
   $("#board .tac").hover(
     function() {
-      if(playerOne && !$(this).hasClass("active")) {
+      if(exes && !$(this).hasClass("active") && playerTurn) {
         $(this).css("background", primaryColor);
         $(this).append("<i class='fa fa-times'></i>");
-      } if(playerTwo && !$(this).hasClass("active")) {
+      } if(ohs && !$(this).hasClass("active") && playerTurn) {
         $(this).css("background", secondaryColor);
         $(this).append("<i class='fa fa-circle-o'></i>");
       }
@@ -36,32 +48,62 @@ $(document).ready(function() {
       }
   });
   $("#board .tac").click(function() {
-    if(playerOne) {
-      $(this).css("background", primaryColor);
-      $(this).text("");
-      $(this).append("<i class='fa fa-times'></i>");
-      $(this).addClass("active p1");
+    if(exes) {
+      if (!computerPlayer) {
+        $(this).css("background", primaryColor);
+        $(this).text("");
+        $(this).append("<i class='fa fa-times'></i>");
+        $(this).addClass("active p1");
 
-      playerOne = false;
-      playerTwo = true;
-      turns++;
-      checkWinner();
-    } else if(playerTwo) {
-      $(this).css("background", secondaryColor);
-      $(this).text("");
-      $(this).append("<i class='fa fa-circle-o'></i>");
-      $(this).addClass("active p2");
+        exes = false;
+        ohs = true;
+        turns++;
+        checkWinner();
+      } else if (computerPlayer && playerTurn) {
+        $(this).css("background", primaryColor);
+        $(this).text("");
+        $(this).append("<i class='fa fa-times'></i>");
+        $(this).addClass("active p1");
 
-      playerOne = true;
-      playerTwo = false;
-      turns++;
-      checkWinner();
+        exes = false;
+        ohs = true;
+        playerTurn = false;
+        turns++;
+        checkWinner();
+        runComputerTurn();
+      }
+    } else if(ohs) {
+      if(!computerPlayer) {
+        $(this).css("background", secondaryColor);
+        $(this).text("");
+        $(this).append("<i class='fa fa-circle-o'></i>");
+        $(this).addClass("active p2");
+
+        exes = true;
+        ohs = false;
+        turns++;
+        checkWinner();
+      } else if (computerPlayer && playerTurn) {
+        $(this).css("background", secondaryColor);
+        $(this).text("");
+        $(this).append("<i class='fa fa-circle-o'></i>");
+        $(this).addClass("active p2");
+
+        exes = true;
+        ohs = false;
+        playerTurn = false;
+        turns++;
+        checkWinner();
+        runComputerTurn();
+      }
     }
   });
+  //click event for restarting a new game after a winner is found
   $("#restart").click(function() {
     clearBoard();
   });
-
+  //function that checks the state of the board and sees if there is a winner
+  //if found, triggers the win event
   function checkWinner() {
     var board = checkBoard();
     console.log(board + " " + turns);
@@ -127,21 +169,84 @@ $(document).ready(function() {
       });
     } else if(player == 1) {
       $("#end").show();
-      $("#end p").text("Xs Win!");
+      $("#end p").text("Exes Win!");
       $("#reset").click(function() {
         clearBoard();
       });
     } else if(player == 2) {
       $("#end").show();
-      $("#end p").text("Circles Win!");
+      $("#end p").text("Ohs Win!");
       $("#reset").click(function() {
         clearBoard();
       });
     }
   }
-  //reset board function
+  //reset board function that reloads the page from the browser cache
   function clearBoard() {
     location.reload(false);
   }
+  //function that handles the computer's move and then changes the board UI to reflect that
+  function runComputerTurn() {
+    var boardState = checkBoard();
+    var bestMove = getMiniMax(boardState);
 
+    if(exes) {
+      /*
+      $("#" + bestMove).css("background", primaryColor);
+      $("#" + bestMove).text("");
+      $("#" + bestMove).append("<i class='fa fa-times'></i>");
+      $("#" + bestMove).addClass("active p1");
+      */
+      console.log("Computer Moves");
+
+      exes = false;
+      ohs = true;
+      playerTurn = true;
+    } else if (ohs) {
+      /*
+      $("#" + bestMove).css("background", secondaryColor);
+      $("#" + bestMove).text("");
+      $("#" + bestMove).append("<i class='fa fa-circle-o'></i>");
+      $("#" + bestMove).addClass("active p2");
+      */
+      console.log("Computer Moves")
+
+      exes = true;
+      ohs = false;
+      playerTurn = true;
+    }
+  }
+  //a function that returns the best move based on the current board state
+  function getMiniMax(boardState) {
+
+  }
 });
+/*
+MinMax Algorithm Psudocode
+-----------------------------
+def minimax(game)
+    return score(game) if game.over?
+    scores = [] # an array of scores
+    moves = []  # an array of moves
+
+    # Populate the scores array, recursing as needed
+    game.get_available_moves.each do |move|
+        possible_game = game.get_new_state(move)
+        scores.push minimax(possible_game)
+        moves.push move
+    end
+
+    # Do the min or the max calculation
+    if game.active_turn == @player
+        # This is the max calculation
+        max_score_index = scores.each_with_index.max[1]
+        @choice = moves[max_score_index]
+        return scores[max_score_index]
+    else
+        # This is the min calculation
+        min_score_index = scores.each_with_index.min[1]
+        @choice = moves[min_score_index]
+        return scores[min_score_index]
+    end
+end
+*/
